0d3bb9f79e0ee666af139e0e8b0da8e9506fac0d
David Denton <denton.david@gmail.com>
2015-12-21 11:05:44 +0000

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#apps
+brew install cask
+brew cask install alfred
+brew cask install crashplan
+brew cask install deeper
+brew cask install flux
+brew cask install google-chrome
+brew cask install intellij
+brew cask install java
+brew cask install jiggler
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text3
+brew cask install tunnelblick
+brew cask install vlc
