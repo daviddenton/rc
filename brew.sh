f1f3d5cc149895de8e79f9d504448a33589795b5
David Denton <daviddenton@users.noreply.github.com>
2016-01-27 08:48:12 +0000

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -12,11 +12,11 @@ brew cask install deluge
 brew cask install flux
 brew cask install google-chrome
 brew cask install intellij
-brew cask install iterm
+brew cask install iterm2
 brew cask install java
 brew cask install jiggler
 brew cask install slack
 brew cask install spotify
-brew cask install sublime-text3
+brew cask install sublime-text
 brew cask install tunnelblick
 brew cask install vlc
